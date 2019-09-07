c03a2e6aae7bb88d9a2c4e602ff6b1800a510f4a
douglas.liang <lianglijian110@hotmail.com>
2019-09-07 23:25:16 +0800

    my Formatter demo in DemoFormatter branch

---
+++ DemoFormatter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manning.MyPhotoAlbum;
+using System.Drawing;
+namespace DemoFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomPhotoFormatter photoFormatter = new CustomPhotoFormatter();
+            Photograph photo = new Photograph(@"D:\red.bmp");
+            Console.WriteLine(string.Format(photoFormatter, "The pixel at (100,100) is {0:p100,100}", photo));
+            Console.ReadKey();
+        }
+    }
+}
